06116ddb75fe9ee2f461409971beaef3d1b2d808
Zeeshan Ahmad <ziishaned@gmail.com>
2025-10-03 12:33:22 -0400

    minor changes

--- mobile/eslint.config.js
+++ mobile/eslint.config.js
@@ -5,35 +5,6 @@ const expoConfig = require("eslint-config-expo/flat");
 module.exports = defineConfig([
   expoConfig,
   {
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parser: require("@typescript-eslint/parser"),
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
-        },
-        project: "./tsconfig.json",
-      },
-    },
-    settings: {
-      "import/resolver": {
-        typescript: {
-          alwaysTryTypes: true,
-          project: "./tsconfig.json",
-        },
-        node: {
-          extensions: [".js", ".jsx", ".ts", ".tsx"],
-        },
-      },
-      "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
-      "import/parsers": {
-        "@typescript-eslint/parser": [".ts", ".tsx"],
-      },
-    },
-  },
-  {
     ignores: ["dist/*"],
   },
 ]);
