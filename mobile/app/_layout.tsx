19e70a6525384c902002c3e6b052442d4b41960c
Zeeshan Ahmad <ziishaned@gmail.com>
2025-10-03 12:57:21 -0400

    minor changes

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,22 +3,19 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ title: "Test strip submissions" }}
-      />
+      <Stack.Screen name="index" options={{ title: "History" }} />
       <Stack.Screen
         name="details"
         options={{
           presentation: "modal",
-          title: "Test strip details",
+          title: "Details",
         }}
       />
       <Stack.Screen
         name="camera"
         options={{
           presentation: "modal",
-          title: "Capture test strip",
+          title: "Camera",
         }}
       />
     </Stack>
